0107f5b59ef27b156c61de3723c4e2f3f004232a
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-24 17:27:53 +0530

    [Ravina] Added:  Method to calculate the average , minimum  and maximum of five random double values.

---
+++ DAY_3_Assignment3/Stats5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_3_Assignment3
+{
+    class Stats5
+    {
+          
+
+        public void CalulateAverage()
+        {
+
+            int n = 5;
+
+            Random random = new Random();
+
+            double N1 = (double)random.NextDouble();
+            double N2 = (double)random.NextDouble();
+            double N3 = (double)random.NextDouble();
+            double N4 = (double)random.NextDouble();
+            double N5 = (double)random.NextDouble();
+
+
+            double average = N1 + N2 + N3 + N4 + N5 / n;
+
+            Console.WriteLine("Average of five random numbers are = " +average);
+            Console.ReadLine();
+        }
+
+    }
+}
