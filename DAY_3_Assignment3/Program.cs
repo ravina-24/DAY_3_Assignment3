a50a324471decabd79972107c39e96f2258a7537
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-24 17:26:52 +0530

    [Ravina] Added : Main Program File

--- DAY_3_Assignment3/Program.cs
+++ DAY_3_Assignment3/Program.cs
@@ -6,14 +6,44 @@ namespace DAY_3_Assignment3
     {
         static void Main(string[] args)
         {
-            CarLoan c = new CarLoan();
-            c.CalculateLoan();
 
-            DayOfWeek d = new DayOfWeek();
-            d.FindDayOfWeek();
+            int input;
+            Console.WriteLine("Enter input = \n" +
+                "1.Calculate loan \n" +
+                "2.FindDayOfWeek\n" +
+                "3.CalculateWindChill\n" +
+                "4.CalculateAverage");
+            input = Convert.ToInt32(Console.ReadLine());
 
-            WindChill w = new WindChill();
-            w.CalculateWindChill();
+            switch (input)
+            {
+                case 1:
+              CarLoan c = new CarLoan();
+              c.CalculateLoan();
+                    break;
+
+                case 2:
+               DayOfWeek d = new DayOfWeek();
+               d.FindDayOfWeek();
+                    break;
+
+                case 3:
+              WindChill w = new WindChill();
+               w.CalculateWindChill();
+                    break;
+
+
+                case 4:
+              Stats5 s = new Stats5();
+              s.CalulateAverage();
+                    break;
+
+                default:                 
+                        Console.WriteLine("Enter proper input");
+                    break;
+            }
+
+
         }
     }
 }
