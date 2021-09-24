bab1c475c407104118c51094ad0c48987c1b6132
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-24 15:48:47 +0530

    [Ravina] Added : Mathod to Find the day of week.

---
+++ DAY_3_Assignment3/DayOfWeek.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_3_Assignment3
+{
+    class DayOfWeek
+    {
+
+
+        public void FindDayOfWeek()
+        {
+
+            Console.WriteLine("---------Finding the day of week--------\n");
+
+
+            int date;
+            Console.Write("Enter the date : \n ");
+            date = Convert.ToInt32(Console.ReadLine());
+
+            int month;
+            Console.Write("Enter the Month: \n  ");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            if ((date >= 1 && date <= 31) && (month >= 1 && month <= 12))
+            {
+               
+                int year;
+                Console.Write("Enter the Year : \n");
+                year = int.Parse(Console.ReadLine());
+
+                int year1 = (year - (14 - month) / 12);
+                int x = (year1 + year1 / 4 - year1 / 100 + year1 / 400);  //checking leap or not 
+                int month1 = (month + 12 * ((14 - month) / 12) - 2);
+
+                int day = ((date + x + 31 * month1 / 12) % 7);
+
+                switch (day)
+                {
+                    case 0:
+                        Console.WriteLine("Sunday");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Monday");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Tuesday");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Wednesday");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thursday");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Friday");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saturday");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input is not valid");
+
+            }
+            Console.ReadLine();
+        }  
+    }
+}
+
+    
+
