5c6b6a4c0c07b74eeb0eb1b3f6e3d35d05772867
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-27 09:49:56 +0530

    [Ravina] Added : Method to find average of five random number and printing maximum and minimum.

--- DAY_3_Assignment3/Stats5.cs
+++ DAY_3_Assignment3/Stats5.cs
@@ -11,21 +11,48 @@ namespace DAY_3_Assignment3
         public void CalulateAverage()
         {
 
+            Console.WriteLine("----------------Calculating the average and finding minimum and maximum ---------------\n");
+
             int n = 5;
+
+            double[] array = new double[n];
 
             Random random = new Random();
 
-            double N1 = (double)random.NextDouble();
-            double N2 = (double)random.NextDouble();
-            double N3 = (double)random.NextDouble();
-            double N4 = (double)random.NextDouble();
-            double N5 = (double)random.NextDouble();
+            for (int i=0;i<n;i++)
+            {
+
+                array[i] = random.NextDouble();
+            }
+
+            double sum = 0.0;
+
+            foreach (var items in array)
+            {
+                sum = sum + items;
+
+            }
+
+            Console.WriteLine("sum of five random numbers =" +sum);
+
+            double averge = sum / 5;
+
+            Console.WriteLine("average of five random number = " +averge);
 
 
-            double average = N1 + N2 + N3 + N4 + N5 / n;
+            Array.Sort(array);
 
-            Console.WriteLine("Average of five random numbers are = " +average);
-            Console.ReadLine();
+            Console.WriteLine("Elements in sorted order are = ");
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            double max = array[n-1];
+            Console.WriteLine("Maximum= " +max);
+         
+            double min = array[0];
+            Console.WriteLine("Minimum= " +min);
         }
 
     }
